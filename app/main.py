04cea9019733f1b0d9828e57d11869d1cfa63b01
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-02-15 01:03:19 +0330

    Update main.py

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from routes import chat_routes, user_routes
+
+
+@asynccontextmanager
+def lifespan():
+    print("App startup")
+    yield
+    print("App shutdown")
+
+
+app = FastAPI(
+    title="Philo-Chat",
+    version="0.1.0",
+    description="Chat with your favorite philosophers - Nietzsche, Socrates, and more-in real-time!",
+    contact={"name": "Erfan Moosavi", "email": "erfanmoosavi84@gmail.com"},
+    license_info={"name": "MIT"},
+    lifespan=lifespan,
+)
+
+app.include_router(chat_routes)
+app.include_router(user_routes)

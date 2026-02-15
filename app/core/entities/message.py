3d83232dc0fec6698384b8872932eb6a0bab55c1
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-02-16 01:08:31 +0330

    Add MessageRole Enum

--- app/core/entities/message.py
+++ app/core/entities/message.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+from enum import Enum
+
+
+class MessageRole(Enum):
+    USER = "user"
+    ASSISTANT = "assistant"
 
 
 class Message:
-    def __init__(self, role: str, author: str, content: str) -> None:
-        self.author = author
+    def __init__(self, role: MessageRole, author: str, content: str) -> None:
         self.role = role
+        self.author = author
         self.content = content
         self.time = datetime.now().strftime("%H:%M")

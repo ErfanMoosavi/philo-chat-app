80beb017d85857dcfcbfca7fc6dc072eb7024be1
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-02-15 10:21:52 +0330

    Use .env variables

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 from services import PhiloChat
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+base_url = os.getenv("BASE_URL")
+model = os.getenv("MODEL")
 
 
 def get_philo_chat() -> PhiloChat:
-    return PhiloChat()
+    return PhiloChat(api_key=api_key, base_url=base_url, model_name=model)

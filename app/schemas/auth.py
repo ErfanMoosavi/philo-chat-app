0707255697b00de40abae703c9948fceecf46cf1
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-02-15 23:52:39 +0330

    fix: bug related to regex

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -4,14 +4,14 @@ from pydantic import BaseModel, Field
 class SignupReq(BaseModel):
     username: str = Field(..., min_length=3, max_length=20, pattern=r"^[a-zA-Z0-9_]+$")
     password: str = Field(
-        ..., min_length=8, max_length=64, pattern=r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$"
+        ..., min_length=8, max_length=64, pattern=r"r'^[A-Za-z0-9!@#$%^&*()_+]+$'"
     )
 
 
 class LoginReq(BaseModel):
     username: str = Field(..., min_length=3, max_length=20, pattern=r"^[a-zA-Z0-9_]+$")
     password: str = Field(
-        ..., min_length=8, max_length=64, pattern=r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$"
+        ..., min_length=8, max_length=64, pattern=r"r'^[A-Za-z0-9!@#$%^&*()_+]+$'"
     )
 
 

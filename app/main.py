f3563969527ecdb6f8cbb4619f26638db69873d3
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-02-22 22:16:53 +0330

    Add middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .routes import auth_routes, chat_routes, user_routes
 from .config import settings
+from .routes import auth_routes, chat_routes, user_routes
 
 
 @asynccontextmanager
@@ -20,6 +21,14 @@ app = FastAPI(
     contact=settings.contact,
     license_info=settings.license_info,
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_routes)

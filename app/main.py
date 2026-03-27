c697eb02b19c91cb61a440bae0332f36d9d2fece
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-03-27 21:50:03 +0330

    Remove lifespan

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
 from .config import settings
 from .routes import auth_routes, chat_routes, user_routes
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # App startup
-    yield
-    # App shutdown
-
 
 app = FastAPI(
     title=settings.app_name,
@@ -19,7 +9,6 @@ app = FastAPI(
     description=settings.description,
     contact=settings.contact,
     license_info=settings.license_info,
-    lifespan=lifespan,
 )
 
 app.include_router(auth_routes)
